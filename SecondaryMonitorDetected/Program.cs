ee25a3a65f2081cbbb150581bf8e78ea7eb37864
Romanito <romain.vallet@gmail.com>
2024-07-02 01:50:19 +0200

    Add project files.

---
+++ SecondaryMonitorDetected/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing;
+using System.Management;
+using System.Reflection;
+using System.Threading;
+using System.Windows.Forms;
+using SecondaryMonitorDetected.Properties;
+
+namespace SecondaryMonitorDetected
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            NotifyIcon icon = new NotifyIcon
+            {
+                Icon = Icon.ExtractAssociatedIcon(Assembly.GetExecutingAssembly().Location),
+                Text = "Affichage secondaire détecté"
+            };
+
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_PnPEntity where service =\"monitor\"");
+
+            while (true)
+            {
+                icon.Visible = searcher.Get().Count > Settings.Default.NbMonitors;
+                Thread.Sleep(Settings.Default.Interval);
+            }
+        }
+    }
+}
